f2cdb7167a735186bc66489b9d50c4529e67a0c9
villa <villa@villadeiMac.local>
2017-11-14 17:37:01 +0800

    Initial Commit

---
+++ TestImg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestImg
+//
+//  Created by villa on 2017/11/14.
+//  Copyright © 2017年 Villaday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestImg/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestImg
+//
+//  Created by villa on 2017/11/14.
+//  Copyright © 2017年 Villaday. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
